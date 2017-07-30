b1f430cc5885810f45e60dde3083da1776481d8f
strkita <kita83@gmail.com>
2017-07-30 17:40:49 +0900

    first commit

---
+++ todo/models.py
@@ -0,0 +1,20 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Tasks(models.Model):
+    task = models.CharField(max_length=200)
+    cre_date = models.DateTimeField('date created')
+    deadline = models.DateTimeField('deadline_date', blank=True)
+    priority = models.CharField(default=0, max_length=1)
+
+    def is_deadline_recently(self):
+        if self.deadline is not None:
+            return self.deadline >= timezone.now() - datetime.timedelta(days=7)
+        else:
+            return None
+
+    def __str__(self):
+        return self.task
